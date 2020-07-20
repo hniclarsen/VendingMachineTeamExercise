40a49bd015ea5c725492bf22029ab86e40679cf8
Heather N Larsen <hlarsen2@paylocity.com>
2020-07-20 15:01:47 -0500

    Add project files.

---
+++ VendingMachine2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace VendingMachine2
+{
+  class Program
+  {
+    static Vending_Machine vendingMachine;
+
+    static void Main(string[] args)
+    {
+      vendingMachine = new Vending_Machine();
+
+      InitializeProducts();
+
+      var flag = true;
+      var line = "";
+
+      while(flag)
+      {
+        line = Console.ReadLine();
+
+        ProcessInput(line);
+      }
+    }
+
+    public static void PrintHelp()
+    {
+      
+    }
+
+    public static void ProcessInput(string input)
+    {
+      if (Double.TryParse(input, out double result))
+      {
+        double amount = Double.Parse(input);
+        vendingMachine.AddCurrency(amount);
+        return;
+      }
+
+      if(String.IsNullOrEmpty(input))
+      {
+        return;
+      }
+
+      var tokens = input.Split(' ');
+      switch (tokens[0])
+      {
+        case "buy":
+          PurchaseItem(tokens[1]);
+          break;
+        default:
+          Console.WriteLine("Unrecognized input");
+          break;
+      }
+    }
+
+    public static void PurchaseItem(string item)
+    {
+      
+    }
+
+    public static void InitializeProducts()
+    {
+      var coke = new Product("Coke", 0.25);
+      var pepsi = new Product("Pepsi", 0.35);
+      var drPepper = new Product("Dr Pepper", 0.45);
+
+      vendingMachine.AddProduct(coke, 10);
+      vendingMachine.AddProduct(pepsi, 10);
+      vendingMachine.AddProduct(drPepper, 10);
+    }
+  }
+}
---
+++ VendingMachine2/Vending Machine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachine2
+{
+  class Vending_Machine
+  {
+    public Dictionary<Product, int> Inventory;
+    public double CurrencyInput { get; private set; }
+
+    public Vending_Machine()
+    {
+      Inventory = new Dictionary<Product, int>();
+      CurrencyInput = 0.00;
+    }
+
+    public void AddCurrency(double input)
+    {
+      CurrencyInput += input;
+    }
+
+    public void AddProduct(Product product, int quantity)
+    {
+      Inventory.Add(product, quantity);
+    }
+
+    public void ResetProductQuantity()
+    {
+      foreach (var product in Inventory)
+      {
+        Inventory[product.Key] = 10;
+      }
+    }
+
+    public void PrintInventory()
+    {
+      foreach (var product in Inventory)
+      {
+        Console.WriteLine($"{product.Key.Name}, Price: {product.Key.Price}");
+      }
+    }
+  }
+}
